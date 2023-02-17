a040bc2884301bb8d8f1beff819574964d6d3568
liyusuo <liyusuo@co-mall.com>
2023-02-17 11:46:17 +0800

    提交2

---
+++ demo/aaa.js
@@ -0,0 +1,12 @@
+console.log("111111111");
+
+const a = 1;
+const b = 2;
+
+export function getNum() {
+  return a + b;
+}
+console.log("aaa", getNum());
+console.log("222222222");
+
+console.log("33333333");
